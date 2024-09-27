408ed5cf3ff4b3d57abde9777386fb49e289dc97
Jackson <162245494+JacksonK01@users.noreply.github.com>
2024-09-27 14:00:01 -0500

    Initial commit

---
+++ src/Images.java
@@ -0,0 +1,18 @@
+import javax.swing.*;
+import java.awt.*;
+
+class Images extends JPanel {
+    private Image backgroundImage;
+
+    public Images(Image backgroundImage) {
+        this.backgroundImage = backgroundImage;
+    }
+
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        if (backgroundImage != null) {
+            g.drawImage(backgroundImage, 0, 0, getWidth(), getHeight(), this);
+        }
+    }
+}
---
+++ src/SubjectOne.java
@@ -0,0 +1,18 @@
+public class SubjectOne {
+    String title;
+    String questionOne;
+    String questionTwo;
+    String questionThree;
+    String questionFour;
+    String questionFive;
+
+    public SubjectOne(String title, String questionOne, String questionTwo, String questionThree,
+                      String questionFour, String questionFive) {
+        this.title = title;
+        this.questionOne = questionOne;
+        this.questionTwo = questionTwo;
+        this.questionThree = questionThree;
+        this.questionFour = questionFour;
+        this.questionFive = questionFive;
+    }
+}
